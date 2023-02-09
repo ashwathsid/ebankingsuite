6eb7767a31d9ca7e520a6c4797a3388c4b444570
ashwathsid <ashwathsid@gmail.com>
2023-02-09 18:54:58 +0530

    EBanking Suite repo

---
+++ src/test/java/com/ebankingsuite/EBankingSuiteTests.java
@@ -0,0 +1,13 @@
+package com.ebankingsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingSuiteTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
